84a420e467c18709a757db74e8a15189c6053a8d
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-10-04 02:06:09 +0700

    Add libmysqlclient-dev in install_mysql.sh

--- rojak-vagrant/install_mysql.sh
+++ rojak-vagrant/install_mysql.sh
@@ -2,5 +2,6 @@ wget http://dev.mysql.com/get/mysql-apt-config_0.8.0-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.0-1_all.deb
 sudo apt-get update
 sudo apt-get install -y mysql-server
+sudo apt-get install -y libmysqlclient-dev
 sudo service mysql status
 
