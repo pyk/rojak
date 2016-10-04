b7414415ceaae057ef7e3edcf802808c63ff564f
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-10-04 22:22:45 +0700

    Delete unused socket

--- rojak-api/lib/rojak_api/endpoint.ex
+++ rojak-api/lib/rojak_api/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule RojakAPI.Endpoint do
   use Phoenix.Endpoint, otp_app: :rojak_api
 
-  socket "/socket", RojakAPI.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
