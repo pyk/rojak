ccea01e393e59f798c89f7bfd3e2353aad783261
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-10-06 23:35:10 +0700

    Update candidate dummy data generator to conform Schema v0.2

--- rojak-database/generate_candidate_data.py
+++ rojak-database/generate_candidate_data.py
@@ -7,7 +7,6 @@ ROJAK_DB_USER = os.getenv('ROJAK_DB_USER', 'root')
 ROJAK_DB_PASS = os.getenv('ROJAK_DB_PASS', 'rojak')
 ROJAK_DB_NAME = os.getenv('ROJAK_DB_NAME', 'rojak_database')
 
-
 # Open database connection
 db = mysql.connect(ROJAK_DB_HOST, ROJAK_DB_USER,
         ROJAK_DB_PASS, ROJAK_DB_NAME)
@@ -16,16 +15,11 @@ db = mysql.connect(ROJAK_DB_HOST, ROJAK_DB_USER,
 cursor = db.cursor()
 
 sql_candidate = '''
-INSERT INTO `candidate`(`name`, `website_url`, `photo_url`,
-    `fbpage_username`, `description`)
-VALUES ('{}', '{}', '{}', '{}', '{}');
-'''
-
-sql_candidate_pair = '''
-INSERT INTO `pair_of_candidates`(`cagub_id`, `cawagub_id`, `name`,
-    `website_url`, `logo_url`, `fbpage_username`, `slogan`,
-    `description`)
-VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');
+INSERT INTO `candidate`(`full_name`, `alias_name`, `place_of_birth`,
+    `date_of_birth`, `religion`, `website_url`, `photo_url`,
+    `fbpage_username`, `twitter_username`, `instagram_username`)
+VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}',
+        '{}', '{}');
 '''
 
 # Create random candidate data
@@ -34,28 +28,41 @@ fake = Factory.create()
 candidate_ids = []
 for i in xrange(MAX_CANDIDATES):
     # Generate random data for the candidates
-    candidate_name = fake.name()
-    website_name = candidate_name.replace(' ', '').lower()
+    full_name = fake.name()
+    alias_name = full_name
+    place_of_birth = 'Indonesia'
+    date_of_birth = '2016-10-01'
+    religion = 'Universe'
+    website_name = full_name.replace(' ', '').lower()
     website_url = 'https://{}.com'.format(website_name)
-    cat_img = 'http://lorempixel.com/500/500/cats/{}'.format(candidate_name)
+    cat_img = 'http://lorempixel.com/500/500/cats/{}'.format(full_name)
     photo_url = cat_img
-    fbpage_username = candidate_name + ' FB'
-    description = fake.text()
+    fbpage_username = website_name
+    twitter_username = website_name
+    instagram_username = website_name
 
     # Parse the SQL command
-    insert_sql = sql_candidate.format(candidate_name, website_url, photo_url,
-        fbpage_username, description)
+    insert_sql = sql_candidate.format(full_name, alias_name, place_of_birth,
+            date_of_birth, religion, website_url, photo_url, fbpage_username,
+            twitter_username, instagram_username)
 
     # insert to the database
     try:
         cursor.execute(insert_sql)
         candidate_id = cursor.lastrowid
-        candidate_ids.append({'id': candidate_id, 'name': candidate_name})
+        candidate_ids.append({'id': candidate_id, 'name': full_name})
         db.commit()
     except mysql.Error as err:
         print("Something went wrong: {}".format(err))
         db.rollback()
 
+# Generate candidate pair
+sql_candidate_pair = '''
+INSERT INTO `pair_of_candidates`(`cagub_id`, `cawagub_id`, `name`,
+    `website_url`, `logo_url`, `fbpage_username`, `slogan`,
+    `description`, `twitter_username`, `instagram_username`)
+VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');
+'''
 is_skip = -1
 for i in xrange(MAX_CANDIDATES):
     if is_skip == i: continue
@@ -70,12 +77,15 @@ for i in xrange(MAX_CANDIDATES):
     cat_img = 'http://lorempixel.com/500/500/cats/{}'.format(website_name)
     logo_url = cat_img
     fbpage_username = website_name
+    twitter_username = website_name
+    instagram_username = website_name
     slogan = 'Slogan {} dan {}'.format(cagub_name, cawagub_name)
     description = fake.text()
 
     # Parse the SQL command
-    insert_sql = sql_candidate_pair.format(cagub_i, cawagub_i, pair_name, website_url,
-            logo_url, fbpage_username, slogan, description)
+    insert_sql = sql_candidate_pair.format(cagub_i, cawagub_i, pair_name,
+            website_url, logo_url, fbpage_username, slogan, description,
+            twitter_username, instagram_username)
 
     # insert to the database
     try:
