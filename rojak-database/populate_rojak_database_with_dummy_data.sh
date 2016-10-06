351beedfe648eb26e995fd483ca2ec0a3db3c63f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-10-06 17:34:51 +0700

    Add script to populate database with dummy data

---
+++ rojak-database/populate_rojak_database_with_dummy_data.sh
@@ -0,0 +1,12 @@
+# Generate candidate data
+python generate_candidate_data.py
+
+# Generate media data
+python generate_media_data.py
+
+# Generate sentiment data
+python generate_sentiment_data.py
+
+# Generate news data, sentiment and mention data
+python generate_news_data.py
+
