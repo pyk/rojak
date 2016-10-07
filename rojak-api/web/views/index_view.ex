0cd51ee86774961a2a20e9cd1b2a83e6bbaef47f
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-10-07 10:46:08 +0700

    Use project version from mix for dummy route

--- rojak-api/web/views/index_view.ex
+++ rojak-api/web/views/index_view.ex
@@ -2,7 +2,7 @@ defmodule RojakAPI.IndexView do
   use RojakAPI.Web, :view
 
   def render("index.json", _assigns) do
-    %{version: "0.0.0", message: "Selamat datang di rojak-api!"}
+    %{version: Mix.Project.config[:version], message: "Selamat datang di rojak-api!"}
   end
 
 end
