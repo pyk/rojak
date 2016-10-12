71bc19403d6afcf93097b848f9fdbf4c21124970
Giri Kuncoro <girikuncoro@gmail.com>
2016-10-11 22:23:02 -0700

    Added parse method for vivanews

--- rojak-pantau/rojak_pantau/spiders/viva.py
+++ rojak-pantau/rojak_pantau/spiders/viva.py
@@ -103,3 +103,46 @@ class VivaSpider(Spider):
                     spider.name, err)
                 self.slack.chat.post_message('#rojak-pantau-errors',
                     error_msg, as_user=True)
+
+    def parse(self, response):
+        self.logger.info('parse: {}'.format(response))
+        is_scraped = False
+
+        # Collect list of news from current page
+        for article in response.css('ul.indexlist > li'):
+            url = article.css('a::attr(href)').extract()[0]
+            # Example: 7 Oktober 2016 19:37
+            info = article.css('div.upperdeck::text').extract()[1]
+            info = info.split(',')[1].replace('\t','').strip()
+
+            # Parse date information
+            try:
+                # Example: 7 October 2016 19:37
+                info_time = info.split(' ')
+                info_time = ' '.join([_(s) for s in info_time])
+                self.logger.info('info_time: {}'.format(info_time))
+                published_at_wib = datetime.strptime(info_time, '%d %B %Y %H:%M')
+                published_at = wib_to_utc(published_at_wib)
+            except Exception as e:
+                raise CloseSpider('cannot_parse_date: {}'.format(e))
+
+            if self.media['last_scraped_at'] >= published_at:
+                is_scraped = True
+                break
+            # For each url we create new scrapy Request
+            yield Request(url, callback=self.parse_news)
+
+        if is_scraped:
+            self.logger.info('Media have no update')
+            return
+
+        # Collect news on next page
+        for tag in response.css('div.pagination > a'):
+            more = tag.css('a::text').extract()[0]
+            if more == 'NEXT':
+                next_page = tag.css('a::attr(href)').extract()[0]
+                next_page_url = response.urljoin(next_page)
+                yield Request(next_page_url, callback=self.parse)
+
+    def parse_news(self, response):
+        pass
