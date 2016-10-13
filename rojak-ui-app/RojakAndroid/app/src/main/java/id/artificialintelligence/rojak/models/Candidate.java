f0ec2137135ff6ae9907a402def67583e880b06c
Mychael Go <admin@mychaelgo.com>
2016-10-13 22:40:40 +0700

    [rojak-app] create model based on rojak api spec (#70)

---
+++ rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/models/Candidate.java
@@ -0,0 +1,107 @@
+package id.artificialintelligence.rojak.models;
+
+/**
+ * Created by mychaelgo on 10/13/16.
+ */
+
+public class Candidate {
+    private int id;
+    private String fullName;
+    private String aliasName;
+    private String placeOfBirth;
+    private String dateOfBirth;
+    private String religion;
+    private String websiteUrl;
+    private String photoUrl;
+    private String fbpageUsername;
+    private String twitterUsername;
+    private String instagramUsername;
+
+    public String getAliasName() {
+        return aliasName;
+    }
+
+    public void setAliasName(String aliasName) {
+        this.aliasName = aliasName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFbpageUsername() {
+        return fbpageUsername;
+    }
+
+    public void setFbpageUsername(String fbpageUsername) {
+        this.fbpageUsername = fbpageUsername;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInstagramUsername() {
+        return instagramUsername;
+    }
+
+    public void setInstagramUsername(String instagramUsername) {
+        this.instagramUsername = instagramUsername;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getTwitterUsername() {
+        return twitterUsername;
+    }
+
+    public void setTwitterUsername(String twitterUsername) {
+        this.twitterUsername = twitterUsername;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+}
