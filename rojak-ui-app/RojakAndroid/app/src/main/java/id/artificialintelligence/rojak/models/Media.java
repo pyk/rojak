8bcc9f862ebd225ce72ac9238bde40c74a3ae04c
mychaelgo <admin@mychaelgo.com>
2016-10-13 17:19:06 +0700

    create model based on rojak api spec

---
+++ rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/models/Media.java
@@ -0,0 +1,71 @@
+package id.artificialintelligence.rojak.models;
+
+/**
+ * Created by mychaelgo on 10/13/16.
+ */
+
+public class Media {
+    private int id;
+    private String name;
+    private String websiteUrl;
+    private String logoUrl;
+    private String fbPageUsername;
+    private String twitterUsername;
+    private String instagramUsername;
+
+    public String getFbPageUsername() {
+        return fbPageUsername;
+    }
+
+    public void setFbPageUsername(String fbPageUsername) {
+        this.fbPageUsername = fbPageUsername;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInstagramUsername() {
+        return instagramUsername;
+    }
+
+    public void setInstagramUsername(String instagramUsername) {
+        this.instagramUsername = instagramUsername;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTwitterUsername() {
+        return twitterUsername;
+    }
+
+    public void setTwitterUsername(String twitterUsername) {
+        this.twitterUsername = twitterUsername;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+}
---
+++ rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/models/News.java
@@ -0,0 +1,53 @@
+package id.artificialintelligence.rojak.models;
+
+/**
+ * Created by mychaelgo on 10/13/16.
+ */
+
+public class News {
+    private int id;
+    private int mediaId;
+    private String title;
+    private String url;
+    private String authorName;
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(int mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
