bde738cf585a34194d417895d770d808dab91cd6
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-10-15 02:04:22 +0700

    [rojak-pantau] Refactor okezone's spider

--- rojak-pantau/rojak_pantau/spiders/okezone.py
+++ rojak-pantau/rojak_pantau/spiders/okezone.py
@@ -1,37 +1,18 @@
 # -*- coding: utf-8 -*-
-import os
 import re
 import json
-import scrapy
-import MySQLdb as mysql
+from datetime import datetime
 
 from scrapy import signals, Request
 from scrapy.exceptions import CloseSpider, NotConfigured
 from scrapy.loader import ItemLoader
 
-from datetime import datetime
-from slacker import Slacker
-
 from rojak_pantau.items import News
 from rojak_pantau.util.wib_to_utc import wib_to_utc
 from rojak_pantau.i18n import _
+from rojak_pantau.spiders.base import BaseSpider
 
-ROJAK_DB_HOST = os.getenv('ROJAK_DB_HOST', 'localhost')
-ROJAK_DB_PORT = int(os.getenv('ROJAK_DB_PORT', 3306))
-ROJAK_DB_USER = os.getenv('ROJAK_DB_USER', 'root')
-ROJAK_DB_PASS = os.getenv('ROJAK_DB_PASS', 'rojak')
-ROJAK_DB_NAME = os.getenv('ROJAK_DB_NAME', 'rojak_database')
-ROJAK_SLACK_TOKEN = os.getenv('ROJAK_SLACK_TOKEN', '')
-
-sql_get_media = '''
-SELECT id,last_scraped_at FROM media WHERE name=%s;
-'''
-
-sql_update_media = '''
-UPDATE `media` SET last_scraped_at=UTC_TIMESTAMP() WHERE name=%s;
-'''
-
-class OkezoneSpider(scrapy.Spider):
+class OkezoneSpider(BaseSpider):
     name="okezone"
     allowed_domains=["okezone.com"]
     start_urls=(
@@ -44,88 +25,17 @@ class OkezoneSpider(scrapy.Spider):
     # This spider will stop once it encounters 5 consecutive empty pages
     failed_count=0
 
-    # Initialize database connection then retrieve media ID and
-    # last_scraped_at information
-    def __init__(self):
-        # Open database connection
-        self.db = mysql.connect(host=ROJAK_DB_HOST, port=ROJAK_DB_PORT,
-            user=ROJAK_DB_USER, passwd=ROJAK_DB_PASS, db=ROJAK_DB_NAME)
-        self.cursor = self.db.cursor()
-
-        self.media = {}
-        try:
-            # Get media information from the database
-            self.logger.info('Fetching media information')
-            self.cursor.execute(sql_get_media, [self.name])
-            row = self.cursor.fetchone()
-            self.media['id'] = row[0]
-            self.media['last_scraped_at'] = row[1]
-        except mysql.Error as err:
-            self.logger.error('Unable to fetch media data: {}'.format(err))
-            raise NotConfigured('Unable to fetch media data: {}'.format(err))
-
-        if ROJAK_SLACK_TOKEN != '':
-            self.is_slack = True
-            self.slack = Slacker(ROJAK_SLACK_TOKEN)
-        else:
-            self.is_slack = False
-            self.logger.info('Post error to #rojak-pantau-errors is disabled')
-
-    # Capture the signal spider_opened and spider_closed
-    # https://doc.scrapy.org/en/latest/topics/signals.html
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super(OkezoneSpider, cls).from_crawler(crawler,
-                *args, **kwargs)
-        crawler.signals.connect(spider.spider_opened,
-                signal=signals.spider_opened)
-        crawler.signals.connect(spider.spider_closed,
-                signal=signals.spider_closed)
-        return spider
-
-    def spider_opened(self, spider):
-        # Using UTF-8 Encoding
-        self.db.set_character_set('utf8')
-        self.cursor.execute('SET NAMES utf8;')
-        self.cursor.execute('SET CHARACTER SET utf8;')
-        self.cursor.execute('SET character_set_connection=utf8;')
-
-    def spider_closed(self, spider, reason):
-        spider.logger.info('Spider closed: %s %s', spider.name, reason)
-        # if spider finished without error update last_scraped_at
-        if reason == 'finished':
-            try:
-                self.logger.info('Updating media last_scraped_at information')
-                self.cursor.execute(sql_update_media, [self.name])
-                self.db.commit()
-                self.db.close()
-            except mysql.Error as err:
-                self.logger.error('Unable to update last_scraped_at: %s', err)
-                self.db.rollback()
-                self.db.close()
-                if self.is_slack:
-                    error_msg = '{}: Unable to update last_scraped_at: {}'.format(
-                        spider.name, err)
-                    self.slack.chat.post_message('#rojak-pantau-errors', error_msg,
-                        as_user=True)
-        else:
-            if self.is_slack:
-                # Send error to slack
-                error_msg = '{}: Spider fail because: {}'.format(
-                    self.name, reason)
-                self.slack.chat.post_message('#rojak-pantau-errors',
-                        error_msg, as_user=True)
-
     def parse(self, response):
         self.logger.info('parse: {}'.format(response))
-        is_scraped = False
+        is_no_update = False
 
         # Collect list of news from current page
-        articles = response.css('li.col-md-12')
-        for article in articles:
+        article_selectors = response.css('li.col-md-12')
+        if not article_selectors:
+            raise CloseSpider('article_selectors not found')
+        for article in article_selectors:
             # Example: http://news.okezone.com/read/2016/10/12/338/1512347/marak-isu-sara-jelang-pilgub-begini-cara-mencegahnya
             url_selectors = article.css('h3 > a::attr(href)')
-
             if not url_selectors:
                 raise CloseSpider('url_selectors not found')
 
@@ -137,42 +47,40 @@ class OkezoneSpider(scrapy.Spider):
 
             # Example: Rabu, 12 Oktober 2016 06:44 WIB
             date_time_str_selectors = article.css('time::text')
-
             if not date_time_str_selectors or len(date_time_str_selectors) < 2:
                 raise CloseSpider('date_time_str_selectors not found')
-
             date_time_str = date_time_str_selectors.extract()[1].strip()
-
             # Parse date information
+            # Example: 12 October 2016 06:44
+            # Remove WIB and convert the date to International based
+            date_time_str = date_time_str.split(',')[1].strip();
+            date_time_str = ' '.join([_(w) for w in date_time_str[:-4].split(' ')])
             try:
-                # Example: 12 October 2016 06:44
-                # Remove WIB and convert the date to International based
-                date_time_str = date_time_str.split(',')[1].strip();
-                date_time_str = ' '.join([_(w) for w in date_time_str[:-4].split(' ')])
-                self.logger.info('info_time: {}'.format(date_time_str))
-                published_at = wib_to_utc(
-                    datetime.strptime(date_time_str, '%d %B %Y %H:%M'))
+                published_at_wib = datetime.strptime(date_time_str,
+                        '%d %B %Y %H:%M')
             except Exception as e:
                 raise CloseSpider('cannot_parse_date: {}'.format(e))
+            published_at = wib_to_utc(published_at_wib)
 
             if (self.media['last_scraped_at'] >= published_at):
-                is_scraped = True
+                is_no_update = True
                 break;
 
             yield Request(url, callback=self.parse_news)
 
-        if is_scraped:
+        if is_no_update:
             self.logger.info('Media have no update')
             return
 
-        if (len(articles) == 0):
+        if (len(article_selectors) == 0):
             self.failed_count += 1
         else:
             self.failed_count = 0
 
         # Collect news on next page
         if response.css('.btn-loadmorenews1 > a'):
-            next_page = response.css('.btn-loadmorenews1 > a::attr(href)').extract()[0]
+            np_selectors = response.css('.btn-loadmorenews1 > a::attr(href)')
+            next_page = np_selectors.extract()[0]
             next_page_url = response.urljoin(next_page)
             yield Request(next_page_url, callback=self.parse)
         elif self.failed_count < 5:
@@ -190,18 +98,12 @@ class OkezoneSpider(scrapy.Spider):
         # Initialize item loader
         # extract news title, published_at, author, content, url
         loader = ItemLoader(item=News(), response=response)
-
         loader.add_value('url', response.url)
 
         if not parsed_news['title']:
             # Will be dropped on the item pipeline
             return loader.load_item()
         loader.add_value('title', parsed_news['title'])
-
-        if not parsed_news['author']:
-            # Will be dropped on the item pipeline
-            return loader.load_item()
-        loader.add_value('author_name', parsed_news['author'])
 
         if not parsed_news['content']:
             # Will be dropped on the item pipeline
@@ -215,15 +117,22 @@ class OkezoneSpider(scrapy.Spider):
             return loader.load_item()
 
         # Parse date information
+        # Example: 12 Oct 2016 - 05:25
+        date_time_str = ' '.join([_(w) for w in parsed_news['published'].split(',')[1].strip()[:-4].split(' ')])
         try:
-            # Example: 12 Oct 2016 - 05:25
-            date_time_str = ' '.join([_(w) for w in parsed_news['published'].split(',')[1].strip()[:-4].split(' ')])
-            self.logger.info('parse_date: parse_news: date_str: %s', date_time_str)
-            published_at = wib_to_utc(
-                            datetime.strptime(date_time_str,'%d %b %Y - %H:%M'))
-            loader.add_value('published_at', published_at)
-        except Exception as e:
-            raise CloseSpider('cannot_parse_date: %s' % e)
+            published_at_wib = datetime.strptime(date_time_str,
+                    '%d %b %Y - %H:%M')
+        except ValueError:
+            # Will be dropped on the item pipeline
+            return loader.load_item()
+        published_at = wib_to_utc(published_at_wib)
+        loader.add_value('published_at', published_at)
+
+        if not parsed_news['author']:
+            loader.add_value('author_name', '')
+        else:
+            loader.add_value('author_name', parsed_news['author'])
 
         # Move scraped news to pipeline
         return loader.load_item()
+
