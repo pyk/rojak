676b997809257303175ecbaab7492fbb6d00b04b
Kevin Luvian <KLUVIAN001@e.ntu.edu.sg>
2016-10-16 12:52:56 +0700

    [rojak-pantau] added fail-safe for merdeka.com (#98)
    * [rojak-pantau] added fail-safe for merdeka.com
    
    * [rojak-pantau] return load_item instead of CloseSpider when failed parse news date

--- rojak-pantau/rojak_pantau/spiders/merdekacom.py
+++ rojak-pantau/rojak_pantau/spiders/merdekacom.py
@@ -21,7 +21,7 @@ class MerdekacomSpider(BaseSpider):
 
     def parse(self, response):
         self.logger.info('parse: {}'.format(response))
-        is_scraped = False
+        is_no_update = False
 
         # Collect list of news from current page
         articles = json.loads(response.body)['response']
@@ -31,26 +31,25 @@ class MerdekacomSpider(BaseSpider):
 
             # Parse date information
             try:
-                self.logger.info('info_time: {}'.format(date_time_str))
-                published_at = wib_to_utc(
-                        datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S'))
+                published_at_wib = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
             except Exception as e:
                 raise CloseSpider('cannot_parse_date: {}'.format(e))
+            published_at = wib_to_utc(published_at_wib)
 
             if (self.media['last_scraped_at'] >= published_at):
-                is_scraped = True
+                is_no_update = True
                 break;
 
             for sub_article in article['news_content']:
                 yield self.parse_news(article, sub_article)
 
-        if is_scraped:
+        if is_no_update:
             self.logger.info('Media have no update')
             return
 
         # Collect news on next page
         if len(articles) > 0:
-            'http://api.merdeka.com/mobile/gettag/pilgub-dki/0/20/L9pTAoWB269T&-E/',
+            # Example: 'http://api.merdeka.com/mobile/gettag/pilgub-dki/0/20/L9pTAoWB269T&-E/'
             next_page_url = response.url.split('/')
             next_page_url[-4] = str(int(next_page_url[-4]) + 20)
             next_page_url = '/'.join(next_page_url)
@@ -58,32 +57,41 @@ class MerdekacomSpider(BaseSpider):
 
     # Collect news item
     def parse_news(self, article, sub_article):
-        if not (sub_article['news_url'] and article['news_title'] and article['news_reporter'] and sub_article['news_description'] and article['news_date_publish']):
-            return
-
         self.logger.info('parse_news: %s' % article)
-
-        # Example: https://m.merdeka.com/tag/p/pilgub-dki/politik/nachrowi-pastikan-agus-sylvi-tak-cuma-incar-suara-santri-ulama.html
-        url = 'https://www.merdeka.com' + sub_article['news_url']
 
         # Initialize item loader
         # extract news title, published_at, author, content, url
         loader = ItemLoader(item=News())
+
+        # Example: https://m.merdeka.com/tag/p/pilgub-dki/politik/nachrowi-pastikan-agus-sylvi-tak-cuma-incar-suara-santri-ulama.html
+        if not sub_article['news_url']:
+            return loader.load_item()
+        url = 'https://www.merdeka.com' + sub_article['news_url']
         loader.add_value('url', url)
+
+        if not article['news_title']:
+            return loader.load_item()
         loader.add_value('title', article['news_title'])
-        loader.add_value('author_name', article['news_reporter'])
+
+        if not article['news_reporter']:
+            loader.add_value('author_name', '')
+        else:
+            loader.add_value('author_name', article['news_reporter'])
+
+        if not sub_article['news_description']:
+            return loader.load_item()
         loader.add_value('raw_content', sub_article['news_description'])
 
         # Parse date information
+        date_time_str = article['news_date_publish']
         try:
             # Example: 2016-10-12 15:16:04
-            date_time_str = article['news_date_publish']
-            self.logger.info('parse_date: parse_news: date_str: %s', date_time_str)
-            published_at = wib_to_utc(
-                        datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S'))
-            loader.add_value('published_at', published_at)
+            published_at_wib = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
         except Exception as e:
-            raise CloseSpider('cannot_parse_date: %s' % e)
+            return loader.load_item()
+
+        published_at = wib_to_utc(published_at_wib)
+        loader.add_value('published_at', published_at)
 
         # Move scraped news to pipeline
         return loader.load_item()
