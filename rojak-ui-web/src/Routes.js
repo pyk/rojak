00611aebbb66cfa50a78b081c33eeffea82836c2
CodeRiderz <chiputera@gmail.com>
2016-10-23 12:54:45 +0700

    add ReduxRouter, fix error on hot-reload (wrong port)

--- rojak-ui-web/src/Routes.js
+++ rojak-ui-web/src/Routes.js
@@ -1,15 +1,12 @@
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import React from 'react';
 
 import Container from './app/utils/Container';
 import HomePage from './app/home/HomePage';
 
-const Routes = () => (
-    <Router history={browserHistory}>
-        <Route component={Container}>
-            <Route path="/(search/:keyword)" component={HomePage}/>
-        </Route>
-    </Router>
-);
-
-export default Routes;
+export default (
+  <Route path='/' component={Container}>
+    <IndexRoute component={HomePage} />
+    <Route path="search/:keyword" component={HomePage} />
+  </Route>
+)
