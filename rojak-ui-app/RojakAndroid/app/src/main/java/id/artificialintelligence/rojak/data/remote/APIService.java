06d710f4db03becca730e681b29d7f48d41ee0c2
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-10-26 21:15:34 +0700

    Add candidates endpoint

--- rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/data/remote/APIService.java
+++ rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/data/remote/APIService.java
@@ -2,12 +2,19 @@ package id.artificialintelligence.rojak.data.remote;
 
 import java.util.List;
 
+import id.artificialintelligence.rojak.models.Candidate;
 import id.artificialintelligence.rojak.models.Media;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import rx.Observable;
 
 public interface ApiService {
+
+    @GET("candidates")
+    Observable<List<Candidate>> getCandidateList();
+
+    @GET("candidates/{id}")
+    Observable<Candidate> getCandidate(@Path("id") int candidateId);
 
     @GET("media")
     Observable<List<Media>> getMediaList();
