4f8b99324bb39d04d28a2555e59c96f1dea5c721
Haydar Ali Ismail <haydar_ai@yahoo.com>
2016-10-26 21:37:40 +0700

    Add pairings endpoint

--- rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/data/remote/APIService.java
+++ rojak-ui-app/RojakAndroid/app/src/main/java/id/artificialintelligence/rojak/data/remote/APIService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import id.artificialintelligence.rojak.models.Candidate;
 import id.artificialintelligence.rojak.models.Media;
 import id.artificialintelligence.rojak.models.News;
+import id.artificialintelligence.rojak.models.PairOfCandidates;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import rx.Observable;
@@ -28,4 +29,10 @@ public interface ApiService {
 
     @GET("news/{id}")
     Observable<News> getNews(@Path("id") int newsId);
+
+    @GET("pairings")
+    Observable<List<PairOfCandidates>> getPairingList();
+
+    @GET("pairings/{id}")
+    Observable<PairOfCandidates> getPairing(@Path("id") int pairingId);
 }
