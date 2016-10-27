39d652a1134400ac000adb26a824b6de4eb016f1
Adi Nugroho <hidrodixtion@gmail.com>
2016-10-27 23:14:13 +0700

    Organize into groups. Added Endpoint and ServiceHelper to simplify request to api.

---
+++ rojak-ui-app/RojakIOS/Rojak/Endpoint.swift
@@ -0,0 +1,16 @@
+//
+//  Endpoint.swift
+//  Rojak
+//
+//  Created by Adi Nugroho on 10/27/16.
+//  Copyright © 2016 Rojak Team. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case pairings
+    case candidates
+    case news
+    case media
+}
