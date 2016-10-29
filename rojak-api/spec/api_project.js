6dc4ae335188bff9c0fd96320a8852ab70e69f3b
windy <reinarduswindy@gmail.com>
2016-10-29 11:31:36 +0700

    fixing order on side nav

--- rojak-api/spec/api_project.js
+++ rojak-api/spec/api_project.js
@@ -4,21 +4,25 @@ define({
   "description": "Rojak API Documentation",
   "apidoc": "0.2.0",
   "order": [
+    "Candidates",
     "GetCandidates",
     "GetCandidate",
     "GetCandidateMediaSentiments",
+    "Pairings",
     "GetPairs",
     "GetPair",
     "GetPairMediaSentiments",
+    "News",
     "GetNews",
     "GetANews",
+    "Media",
     "GetMedia",
     "GetAMedia"
   ],
   "sampleUrl": false,
   "generator": {
     "name": "apidoc",
-    "time": "2016-10-29T03:07:29.568Z",
+    "time": "2016-10-29T04:29:43.267Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
