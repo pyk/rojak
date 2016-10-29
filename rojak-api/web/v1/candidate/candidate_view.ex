e18380881b334f32adccb04b428e868a3751a081
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-10-29 21:42:37 +0700

    Enable embedding candidate sentiments

--- rojak-api/web/v1/candidate/candidate_view.ex
+++ rojak-api/web/v1/candidate/candidate_view.ex
@@ -12,7 +12,7 @@ defmodule RojakAPI.V1.CandidateView do
   def render("candidate.json", %{candidate: candidate}) do
     candidate =
       candidate
-      |> Map.drop([:__meta__, :mentioned_in, :sentiments])
+      |> Map.drop([:__meta__, :mentioned_in])
 
     # Embed pairing
     candidate = case Map.get(candidate, :pairing) do
@@ -21,6 +21,20 @@ defmodule RojakAPI.V1.CandidateView do
       pairing ->
         Map.update! candidate, :pairing, fn _ ->
           Map.drop(pairing, [:__meta__, :cagub, :cawagub])
+        end
+    end
+
+    # Embed sentiments
+    candidate = case Map.get(candidate, :sentiments) do
+      nil ->
+        candidate |> Map.drop([:sentiments])
+      sentiments ->
+        Map.update! candidate, :sentiments, fn _ ->
+            %{
+              positive: sentiments.positive,
+              neutral: sentiments.neutral,
+              negative: sentiments.negative,
+            }
         end
     end
 
