741a1a5522f95820de6ad2242d755d25065164cf
Yuri Pratama <hello@yuripertamax.me>
2016-10-31 23:11:32 +0700

    Prepare for prototyping result with uikit

--- rojak-ui-web/src/app/result/DefaultResult.js
+++ rojak-ui-web/src/app/result/DefaultResult.js
@@ -6,7 +6,10 @@ const DefaultResult = () => {
   return (
     <ResultGateway hideIn={['media', 'kandidat', 'pasangan']}>
       <div className={styles.resultWrapper}>
-        THIS IS DEFAULT RESULT
+      THIS IS DEFAULT RESULT
+        <div className="uk-grid-collapse" style={{ paddingTop: '20px' }}>
+          <img className="uk-width-1-5 uk-thumbnail" src="http://placehold.it/100x100"/>
+        </div>
       </div>
     </ResultGateway>
   )
